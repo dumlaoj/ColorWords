e8e89981fc86720815f8fc0e76e7091ca2a6f12c
Jordan <dumlaoj@hawaii.edu>
2019-01-19 11:56:07 -0800

    Remove storyboard and add Game View Controller

---
+++ ColorWords/GameViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GameViewController.swift
+//  ColorWords
+//
+//  Created by Jordan Dumlao on 1/19/19.
+//  Copyright © 2019 Jordan Dumlao. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+}
